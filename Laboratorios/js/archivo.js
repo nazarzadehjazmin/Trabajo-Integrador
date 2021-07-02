86386012d14ef1da12a89a8a8f83522e384edce2
nazarzadehjazmin <nazarzadehjazmin@gmail.com>
2021-07-01 23:26:22 -0300

    laboratorios

---
+++ Laboratorios/js/archivo.js
@@ -0,0 +1,9 @@
+function abrir() {
+    document.getElementById('formulario').style.display = 'block';
+}
+
+function cerrar() {
+    document.getElementById('formulario').style.display = 'none';
+  
+} 
+
